ec4036cad5295f3ca696b34f30c5a1d50ce9a7c6
youyou87 <47578612+youyou87@users.noreply.github.com>
2020-05-20 02:17:33 +0200

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+class Classroom {
+   
+  public  static void main (String [] arg){
+
+  
+  Wilder jean = new Wilder ("toto " , true);
+     System.out.println(toto.whoAmI());
+  Wilder Truc = new Wilder ("titi " , false );
+     System.out.println (titi.whoAmI());
+  }
+}
